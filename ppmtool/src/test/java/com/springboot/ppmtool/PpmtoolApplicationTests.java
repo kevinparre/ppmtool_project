b954fd5d93da573d5e0c6397be088045294734b7
kevinparre <parrekevin@gmail.com>
2022-04-25 23:31:05 +0530

    First Commit

---
+++ ppmtool/src/test/java/com/springboot/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
