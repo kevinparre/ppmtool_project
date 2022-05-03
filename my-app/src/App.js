4c1ac587b432f287631d639292c271c44f656ef5
kevinparre <parrekevin@gmail.com>
2022-05-03 20:26:24 +0530

    Redux unsolved errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,29 +3,38 @@ import './App.css';
 import DashBoards from './components/DashBoards';
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router ,Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router ,Routes, Route, BrowserRouter} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import { Provider } from "react-redux";
+import store from "./store";
 
+import history from "./history";
 
 class App extends Component{
   render() {
 return (
   
-  
+  <Provider store={store}>
+  <Router history={history}>
     <div className="App">
     
-    <Router>
+    
     <Header />
     
     <Routes>
+    <Route exact path="/" component={DashBoards} />
     <Route path="/dashboard.html" element={<DashBoards />} />
     <Route path="/dashboard" element={<DashBoards />} />
     <Route exact path="/addProject" element={<AddProject />} />
     
    
     </Routes>
-    </Router>
+    
+    
     </div>
+    </Router>
+    </Provider>
+ 
 
 
     
---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import errorReducer from "./errorReducer";
+
+
+const rootReducer= combineReducers({
+  errors: errorReducer
+});
+
+export default rootReducer
